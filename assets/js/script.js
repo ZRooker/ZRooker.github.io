ac1337e7e939d2b4e2bb906395343821a5efd842
ZR <zarekrook@gmail.com>
2023-11-13 18:50:30 -0600

    Fix?

--- assets/js/script.js
+++ assets/js/script.js
@@ -82,11 +82,3 @@
 		}
 	});
 
-        
-//<!-- Google tag (gtag.js) -->
-
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'G-ZMZSTBQJN7');
